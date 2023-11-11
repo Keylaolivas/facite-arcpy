cdcf0f164482df31e475c0fc74cb03cbdf2e8959
prac_1_key <keylaolivas27@gmail.com>
2023-11-10 17:04:42 -0700

    arcpy

--- arcpy/muni_vecinos.py
+++ arcpy/muni_vecinos.py
@@ -2,16 +2,23 @@
 import arcpy
 import csv
 arcpy.env.overwriteOutput = True
-municipios = r'C:\Users\FOTO_03\PycharmProjects\facite-arcpy\muni2018\muni_2018cw.shp'
-municipios_csv = r'C:\Users\FOTO_03\PycharmProjects\facite-arcpy\muni2018\municipios.csv'
+municipios = arcpy.GetParameterAsText(0)#r'C:\Users\FOTO_03\PycharmProjects\facite-arcpy\muni2018\muni_2018cw.shp'
+municipios_csv = arcpy.GetParameterAsText(1) #r'C:\Users\FOTO_03\PycharmProjects\facite-arcpy\muni2018\municipios.csv'
+
 arcpy.MakeFeatureLayer_management(municipios, "municipios_capa")
 total_muni = arcpy.GetCount_management("municipios_capa")[0]
 lista_excel = []
+arcpy.AddMessage('Total de municipios a analizar : {0}'.format(total_muni))
 print total_muni
-for i in range(0,  int(5)):
+arcpy.SetProgressor("step", "Analizando informacion...", 0, int(total_muni), 1 )
+
+
+for i in range(0,  int(total_muni)):
 
     FID = "FID =" + str(i)
     seleccionado = arcpy.SelectLayerByAttribute_management("municipios_capa ", "NEW_SELECTION", FID)
+    arcpy.SetProgressorLabel("Cargando vecinos del municipio ")
+    NOMBRE_MUNICIPIO_seleccionado = ""
 
     for columna in arcpy.da.SearchCursor(seleccionado, ["NOM_MUN", "NOM_ENT"]):
         NOMBRE_MUNICIPIO_seleccionado = columna[0].encode('utf-8')
@@ -19,20 +26,27 @@ for i in range(0,  int(5)):
         print ("{0} - Estado: {1} ---------- Minicipio: {2}".format(i, NOMBRE_ESTADO, NOMBRE_MUNICIPIO_seleccionado))
         #crear una capa temporal de municipios
         muni_vecinos_capa = arcpy.SelectLayerByLocation_management("municipios_capa", "INTERSECT", seleccionado)
-        for vecino in muni_vecinos_capa:
-            for columna in arcpy.da.SearchCursor(vecino, ["FID", "NOM_MUN", "NOM_ENT"]):
-                NOMBRE_MUNICIPIO = columna[1].encode('utf-8')
-                NOMBRE_ESTADO = columna[2].encode('utf-8')
-                print ("vecino {0} - Estado: {1} ---------- Minicipio: {2}".format(i, NOMBRE_ESTADO, NOMBRE_MUNICIPIO_seleccionado))
-                fila = list(columna)
-                fila.append(NOMBRE_MUNICIPIO_seleccionado)
-                lista_excel.append((fila))
+        for columna in arcpy.da.SearchCursor(muni_vecinos_capa, ["FID", "NOM_MUN", "NOM_ENT"]):
+            NOMBRE_MUNICIPIO = columna[1].encode('utf-8')
+            NOMBRE_ESTADO = columna[2].encode('utf-8')
+            print ("vecino {0} - Estado: {1} ---------- Municipio: {2}".format(i, NOMBRE_ESTADO, NOMBRE_MUNICIPIO_seleccionado))
+            fila = list([columna[0], columna[1].encode('utf-8'), columna[2].encode('utf-8') ])
+            fila.append(NOMBRE_MUNICIPIO_seleccionado)
+            lista_excel.append(fila)
+        arcpy.SetProgressorLabel("Cargando vecinos del municipio " + NOMBRE_MUNICIPIO_seleccionado)
+
+    del muni_vecinos_capa
+    del seleccionado
+    arcpy.SetProgressorPosition(i)
+
 with open (municipios_csv, "wb") as archivo:
     csvwriter= csv.writer (archivo, delimiter=",")
     cabecera  = ["FID", "NOM_MUN", "NOM_ENT", "MUN_SELECCIONADO"]
     csvwriter.writerow(cabecera)
     for fila in lista_excel:
         csvwriter.writerow(fila)
-        print "proceso finalizado"
-        k
+print "proceso finalizado"
+arcpy.AddMessage("proceso finalizado")
+#ResetProgressor()
+
 
